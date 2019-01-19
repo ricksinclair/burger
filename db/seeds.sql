7a1aebec38d70739adfa9c7f1e9f3eddab3054ce
Ulric Todman <rick.sinclair.nyc@gmail.com>
2019-01-19 04:31:30 -0500

    added sql files

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+/* Seeds for SQL table. We haven't discussed this type of file yet */
+USE burgers_db;
+
+/* Insert 3 Rows into your new table */
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Whooper", false),
+    ("Big Mac", false),
+    ("Big Buford", false),
+    ("Dave's Double", false),
+    ("Baconator", false),
+    ("Jr Bacon Cheeseburger", false);
